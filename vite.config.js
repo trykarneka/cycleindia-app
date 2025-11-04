c5aba99910b12722ca180261fe3e2d8f4695610e
trykarneka <anurag1990june21@gmail.com>
2025-11-04 14:40:44 +0000

    new commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: [".csb.app", ".codesandbox.io"],
+    host: true,
     port: 5173,
-  }
-})
+  },
+});
